023734fccf6250a999225197eb0e833745bc02cb
Vkadivostok <221219834@mail.ru>
2025-03-16 12:29:54 +0300

    добавил меню дя мобильных устройств

--- send_email.php
+++ send_email.php
@@ -1,29 +1,24 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Получаем данные из формы
     $name = strip_tags(trim($_POST["name"]));
     $phone = strip_tags(trim($_POST["phone"]));
     $message = strip_tags(trim($_POST["message"]));
     $captcha = intval($_POST["captcha"]);
-
-    // Проверяем капчу
+    
     if ($captcha !== 8) {
         http_response_code(400);
         echo "Ошибка: неверный ответ на капчу.";
         exit;
     }
-
-    // Указываем адрес получателя
-    $to = "advokat_kv@mail.ru"; // Замените на вашу почту
+    
+    $to = "advokat_kv@mail.ru";
     $subject = "Новая заявка от $name";
     $email_content = "Имя: $name\n";
-    $email_content .= "Телефон: $phone\n\n";
+    $email_content .= "Телефон: $phone\n";
     $email_content .= "Сообщение:\n$message\n";
-
-    // Заголовки письма
+    
     $headers = "From: $name <$phone>";
-
-    // Отправляем письмо
+    
     if (mail($to, $subject, $email_content, $headers)) {
         http_response_code(200);
         echo "Спасибо! Ваше сообщение отправлено.";
